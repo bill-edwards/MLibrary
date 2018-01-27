33e3fb4947ec5550c1fde0f77d49725b1d49aff9
Bill Edwards <winner.bill@gmail.com>
2018-01-27 14:21:37 +0000

    Cross-entropy cost

--- costs.py
+++ costs.py
@@ -16,3 +16,21 @@ class LeastSquares(object):
 		difference_vector = self.training_outputs - self.model_function(self.training_inputs, parameters)
 		derivative = np.transpose(self.training_inputs).dot(difference_vector)
 		return derivative
+
+class CrossEntropy(object):
+
+	def __init__(self, training_inputs, training_outputs, model_function):
+		self.training_inputs = training_inputs
+		self.training_outputs = training_outputs
+		self.model_function = model_function
+
+	def cost_function(self, parameters):
+		predicted_outputs = self.model_function(self.training_inputs, parameters)
+		cost_vector = (-self.training_outputs * np.log(predicted_outputs)) - ((1 - self.training_outputs) * np.log(1 - predicted_outputs))
+		cost = cost_vector.sum
+		return cost
+
+	def first_derivative(self, parameters):
+		difference_vector = self.training_outputs - self.model_function(self.training_inputs, parameters)
+		derivative = np.transpose(self.training_inputs).dot(difference_vector)
+		return derivative
